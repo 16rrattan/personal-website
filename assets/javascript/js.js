238dc18754b67968610fdb2bac8604b0a3c0430f
Richard Rattan <16rrattan@gmail.com>
2019-07-10 10:43:43 -0400

    this is the first draft of my personal website

---
+++ assets/javascript/js.js
@@ -0,0 +1,13 @@
+$("#linkedin").on("click ", function() {
+
+    window.open("https://www.linkedin.com/in/richard-rattan/")
+
+
+});
+
+$("#github").on("click ", function() {
+
+    window.open("https://github.com/16rrattan")
+
+
+});
